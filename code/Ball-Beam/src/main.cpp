e90904e52ff22d85a01e32dd60e4519ca6f9f3bd
Jacob Lessing <jacob_lessing@outlook.com>
2025-02-15 14:55:11 -0500

    init code commit

--- code/Ball-Beam/src/main.cpp
+++ code/Ball-Beam/src/main.cpp
@@ -1,15 +1,7 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
 }
